475092833646060fc339941a61a39d01c1ea8884
Francesco Imparato <FraImpa10@gmail.com>
2024-01-19 13:43:32 +0100

    aggiunto label e bottoni in Slot

---
+++ src/main/java/com/example/casino3/CasinoController.java
@@ -0,0 +1,24 @@
+package com.example.casino3;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CasinoController {
+    @FXML
+    private Label Risultato;
+
+
+    @FXML
+    private Label importoGiocato;
+
+
+    @FXML
+    protected void OnGiocaSlotButtonClick(){
+
+    }
+
+    public void OnCambiaImportoButtonClick(ActionEvent actionEvent) {
+
+    }
+}
