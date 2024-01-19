49e33c7464d41a2e72ba33537978a19562aeefb4
FerriFilippo <filippo.ferri.006@gmail.com>
2024-01-19 12:28:14 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.casino3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.casino3 to javafx.fxml;
+    exports com.example.casino3;
+}
