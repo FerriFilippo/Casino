5c1734546f59dc0b3fe4e93c8b97de2a290d6692
Muhammad Shayan <m.shayanpc@gmail.com>
2024-01-19 13:43:52 +0100

    Caricamento delle immagini 1.0

--- src/main/java/com/example/casino3/CasinoApplication.java
+++ src/main/java/com/example/casino3/CasinoApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class CasinoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CasinoApplication.class.getResource("casinò-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Casinò");
         stage.setScene(scene);
         stage.show();
     }
