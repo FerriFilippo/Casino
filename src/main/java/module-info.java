9c44df52a0ca294b0dce849ad49268fa91deac65
Francesco Imparato <FraImpa10@gmail.com>
2024-01-26 13:31:55 +0100

    aggiunto label e bottoni in Slot

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.casino3 {
                             
     opens com.example.casino3 to javafx.fxml;
     exports com.example.casino3;
+    exports Slot;
+    opens Slot to javafx.fxml;
 }
