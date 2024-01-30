b14c4191a2bd06c1b0365e4c3c79cec570287b59
Francesco Imparato <FraImpa10@gmail.com>
2024-01-30 12:20:34 +0100

    terminata la pagina con con label e botton

--- src/main/java/com/example/casino3/CasinoApplication.java
+++ src/main/java/com/example/casino3/CasinoApplication.java
@@ -12,7 +12,7 @@ public class CasinoApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CasinoApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("ciao!");
+        stage.setTitle("hello!");
         stage.setScene(scene);
         stage.show();
     }
